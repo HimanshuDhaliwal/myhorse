765e56985809705d3e16c2a35e40bd65141bfadb
Himanshu <94973563+HimanshuDhaliwal@users.noreply.github.com>
2024-08-18 19:23:22 +0530

    Add files via upload

---
+++ HORSE-2.0/README.md
@@ -0,0 +1,3 @@
+This is Horse portfolio website 
+version 2.0
+Using Express and Node.js with FIREBASE enabled and Google Analytics
---
+++ HORSE-2.0/app.js
@@ -0,0 +1,43 @@
+// const express = require('express');
+// const path=require('path');
+// const app = express();
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'home.html'));
+// });
+// app.get('/home', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'home.html'));
+// });
+// app.get('/aboutUs', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'aboutUs.html'));
+// });
+// app.get('/blog', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'blog.html'));
+// });
+// app.get('/contactUs', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'contactUs.html'));
+// });
+// app.get('/faqs', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'FAQs.html'));
+// });
+// app.get('/terms', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'tnc.html'));
+// });
+// app.get('/privacy', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'privacy.html'));
+// });
+
+
+
+
+
+// app.use((req,res)=>{
+//     res.status(404);
+//     res.send(`<h1> ERROR 404 PAGE NOT FOUND<h1>`)
+// })
+
+// app.listen(9005, ()=>{
+//     console.log('listening on port 9005');
+// })
